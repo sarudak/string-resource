c01b16af7bb87efa0c8c0c540d1ba98bb8734fe2
sarudak <sarudak@gmail.com>
2016-04-08 16:15:13 -0700

    Working get

--- src/string_resource/handler.clj
+++ src/string_resource/handler.clj
@@ -5,17 +5,28 @@
 
 (defprotocol AStringResourceStore
   "An interface for storing string resources by langauge"
-  (store [key language] "Stores a string resource")
-  (retrieve [keys languages]
+  (store [this key language value] "Stores a string resource")
+  (retrieve [this keys languages]
     "Retrieves all string resources associated with the given keys and languages"))
 
-(def ^:dynamic string-store nil)
+(defrecord MockStringResourceStore [state]
+  AStringResourceStore
+  (store [this key language value] (swap! state #(assoc % [key language] value)))
+  (retrieve [this keys languages] (@(:state this) [(first keys) (first languages)])))
 
-(defn get-string [key language] (retrieve string-store [key] [language]))
+(def ^:dynamic *string-store* (MockStringResourceStore. (atom {["the-id" "language"] "You found me"})))
+
+(defn get-string [key language]
+  (let [string-resource (retrieve *string-store* [key] [language])]
+    (if string-resource
+      string-resource
+      {:status 404
+        :body "String resource not available"})))
+;(defn get-string [key language] (retrieve string-store [key] [language]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/string/:key/:language" [key language] )
+  (GET "/string/:key/:language" [key language] (get-string key language))
   (route/not-found "Not Found"))
 
 (def app
--- test/string_resource/handler_test.clj
+++ test/string_resource/handler_test.clj
@@ -3,6 +3,18 @@
             [ring.mock.request :as mock]
             [string-resource.handler :refer :all]))
 
+(deftest test-get
+  (testing "Nonexistent resource"
+    (let [response (app (mock/request :get "/string/not-real/en-US"))]
+      (is (= (:status response) 404))
+      (is (= (:body response) "String resource not available"))))
+  (testing "Existing resource is returned"
+    (binding [*string-store* (->MockStringResourceStore
+      (atom {["the-id" "language"] "You found me"}))]
+      (let [response (app (mock/request :get "/string/the-id/language"))]
+        (is (= (:status response) 200))
+        (is (= (:body response) "You found me"))))))
+
 (deftest test-app
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
