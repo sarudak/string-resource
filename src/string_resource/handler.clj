a9a2a4c4c61b090617a316a1aee263091f215351
sarudak <sarudak@gmail.com>
2016-04-08 19:10:29 -0700

    Got the put working

--- src/string_resource/handler.clj
+++ src/string_resource/handler.clj
@@ -1,7 +1,8 @@
 (ns string-resource.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.json :refer [wrap-json-params]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defprotocol AStringResourceStore
   "An interface for storing string resources by langauge"
@@ -14,20 +15,27 @@
   (store [this key language value] (swap! state #(assoc % [key language] value)))
   (retrieve [this keys languages] (@(:state this) [(first keys) (first languages)])))
 
-(def ^:dynamic *string-store* (MockStringResourceStore. (atom {["the-id" "language"] "You found me"})))
+(def ^:dynamic *string-store* (MockStringResourceStore. (atom {})))
 
-(defn get-string [key language]
+(defn get-resource [key language]
   (let [string-resource (retrieve *string-store* [key] [language])]
     (if string-resource
       string-resource
       {:status 404
         :body "String resource not available"})))
-;(defn get-string [key language] (retrieve string-store [key] [language]))
+
+(defn set-resource [key language value]
+  (store *string-store* key language value)
+  {:status 200
+    :body true})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
-  (GET "/string/:key/:language" [key language] (get-string key language))
+  (GET "/string/:key/:language" [key language] (get-resource key language))
+  (PUT "/string/:key/:language" [key language value] (set-resource key language value))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (->
+    (wrap-defaults app-routes api-defaults)
+    wrap-json-params))
--- test/string_resource/handler_test.clj
+++ test/string_resource/handler_test.clj
@@ -1,16 +1,36 @@
 (ns string-resource.handler-test
   (:use midje.sweet)
   (:require [ring.mock.request :as mock]
-            [string-resource.handler :refer :all]))
+            [string-resource.handler :refer :all]
+            [cheshire.core :as json]))
+
+(defmacro let-with-seed [string-store-seed & body]
+  `(binding [*string-store* (->MockStringResourceStore (atom ~string-store-seed))]
+      (let ~@body)))
 
 (fact "gets to /string get string-resources"
   (fact "when a resource does not exist the service returns 404"
-    (let [response (app (mock/request :get "/string/not-real/en-US"))]
+    (let-with-seed {} [response (app (mock/request :get "/string/not-real/en-US"))]
       (:status response) => 404
       (:body response) => "String resource not available"))
   (fact "when a resource does exist it is returned with a 200"
-    (binding [*string-store* (->MockStringResourceStore
-      (atom {["the-id" "language"] "You found me"}))]
-      (let [response (app (mock/request :get "/string/the-id/language"))]
-        (:status response) => 200
-        (:body response) => "You found me"))))
+    (let-with-seed {["the-id" "language"] "You found me"}
+      [response (app (mock/request :get "/string/the-id/language"))]
+      (:status response) => 200
+      (:body response) => "You found me")))
+
+(defn mock-put [key language value]
+    (->
+      (mock/request :put (str "/string/" key "/" language)
+        (json/generate-string {"value" value}))
+      (mock/content-type "application/json")))
+
+(fact "puts to /string change string resources"
+  (fact "when a resource does not exist the resource is created"
+    (let-with-seed {}
+      [put-response (app (mock-put "new-key" "en-US" "This is the resource string"))
+       get-response (app (mock/request :get "/string/new-key/en-US"))]
+        (:status put-response) => 200
+        (:body put-response) => true
+        (:status get-response) => 200
+        (:body get-response) => "This is the resource string")))
